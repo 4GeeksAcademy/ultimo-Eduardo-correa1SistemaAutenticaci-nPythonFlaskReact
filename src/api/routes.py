c8375adbbfdb8d2e0b677cbb89cd1c81fb0e73e2
Eduardo Correa <159133054+Eduardo-correa1@users.noreply.github.com>
2024-08-29 18:10:03 +0000

    instalacion jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
@@ -20,3 +20,12 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    body = request.json
+    user = User.query.filter_by(email=body["email"]).first()
+    if not user:
+        return jsonify({"msg":"usuario o contraseña no validos"})
+    access_token = create_access_token(identity=user.serialize())
+    return jsonify({"token":access_token})
